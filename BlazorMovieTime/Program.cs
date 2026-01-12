2de6c8f180a77b8bebc953a538fda09fa5997848
Munteanu Silviu-Cristian <9718866 +munteanusil@users.noreplay.github.com>
2026-01-11 23:22:20 -0800

    TMDB API Service

--- BlazorMovieTime/Program.cs
+++ BlazorMovieTime/Program.cs
@@ -1,4 +1,5 @@
 using BlazorMovieTime;
+using BlazorMovieTime.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<BlazorMovieTime.Components.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<TMDBClient>();
 
 await builder.Build().RunAsync();
